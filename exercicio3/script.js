619fd624e46a0fdaf398cf228bb405618dd3951b
vitorfontenele <vitor_fontenele@hotmail.com>
2022-09-12 12:14:46 -0300

    Exercício 3 realizado

---
+++ exercicio3/script.js
@@ -0,0 +1,15 @@
+const maioresBaleias = [
+  "Baleia-Azul",
+  "Fin",
+  "Baleia-da-Groenlândia",
+  "Sei",
+  "Baleia-Franca-do-Norte",
+];
+
+/* fonte:
+https://super.abril.com.br/mundo-estranho/quais-sao-as-maiores-baleias-do-mundo/
+*/
+
+for (let i = 0; i < maioresBaleias.length; i++) {
+  console.log(`${i + 1} - ${maioresBaleias[i]}`);
+}
