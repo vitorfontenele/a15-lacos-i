6f324353e02b5ca3ee2256cf2640b98f498c011c
vitorfontenele <vitor_fontenele@hotmail.com>
2022-09-12 15:20:34 -0300

    Exercício 1 atualizado

--- exercicio1/script.js
+++ exercicio1/script.js
@@ -1,14 +1,15 @@
 let conta = 0;
-let resposta = prompt("Deseja comer uma coxinha?");
+let resposta = prompt("Deseja comer uma coxinha [S/N]?").toUpperCase();
+let preco = 2.5;
 
-while (resposta == "S"){
-    conta += 2.5;
-    resposta = prompt("Deseja comer mais uma coxinha?");
-    if (resposta != "S" && resposta != "N"){
-        alert("Apenas S ou N são respostas válidas!");
-        resposta = "S";
-        conta -= 2.5;
-    }
+while (resposta !== "N") {
+  if (resposta === "S") {
+    conta += preco;
+    resposta = prompt("Deseja comer mais uma coxinha [S/N]?").toUpperCase();
+  } else {
+    alert("Apenas S ou N são respostas válidas!");
+    resposta = prompt("Deseja comer mais uma coxinha [S/N]?").toUpperCase();
+  }
 }
 
 console.log(`R$ ${conta}`);
